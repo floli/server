6772808a60c8c94cf507ac6d42a0b48f6b72d87e
Florian Lindner <florian.lindner@xgm.de>
2018-09-02 20:13:13 +0200

    Some fixes, use pathlib.chmod.

--- admin/policyChecker.py
+++ admin/policyChecker.py
@@ -5,8 +5,8 @@ def checkSSLKey(key, user):
     """ Checks if the SSL key are correct, sends email if not."""
     mode = key.stat().st_mode
     if (mode & stat.S_IRGRP) or (mode & stat.S_IROTH):
-        os.chmod(key, stat.S_IRUSR | stat.S_IWUSR)
+        key.chmod(stat.S_IRUSR | stat.S_IWUSR)
         text = """Dear {user},
 Your SSL key {key} had permissions that made it group and world readable. This was corrected and permissions were set to -rw-------. However, there was a time period when it was world readable.
 Yours sincerely, root.""".format(user = user, key = key)
-        mail_root(user, "Security violation found for %s" % user, text)
+        mail_user(user, "Security violation found for %s" % user, text)
